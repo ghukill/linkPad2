bf81ed3595839e0dc13887e12ae0ca36ee49b88e
root <root@mitten.(none)>
2014-03-26 16:39:05 +0000

    fixed missing urllib2 dependency

--- linkPad2/linkPad2/views.py
+++ linkPad2/linkPad2/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from bs4 import BeautifulSoup
+import urllib2
 import datetime
 import json
 import sunburnt
